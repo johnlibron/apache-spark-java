385030d762fd0c1f8c10163bc0eae597a7ddc6a5
John James Libron <johnjames.libron@gmail.com>
2022-08-23 14:29:23 +0800

    Add logger

--- src/main/java/com/virtualpairprogrammers/rddv2/WordCount.java
+++ src/main/java/com/virtualpairprogrammers/rddv2/WordCount.java
@@ -1,5 +1,7 @@
 package com.virtualpairprogrammers.rddv2;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -11,6 +13,7 @@ public class WordCount {
 
     public static void main(String[] args) {
 
+        Logger.getLogger("org").setLevel(Level.ERROR);
         SparkConf conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
