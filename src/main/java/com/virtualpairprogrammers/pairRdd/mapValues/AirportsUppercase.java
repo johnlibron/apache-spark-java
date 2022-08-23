6f54509244b74a73ef4a804b5706141fa9b369bf
John James Libron <johnjames.libron@gmail.com>
2022-08-23 17:42:55 +0800

    Add sample of pair rdd with map values toUppercase

---
+++ src/main/java/com/virtualpairprogrammers/pairRdd/mapValues/AirportsUppercase.java
@@ -0,0 +1,43 @@
+package com.virtualpairprogrammers.pairRdd.mapValues;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class AirportsUppercase {
+
+    private static final String COMMA_DELIMITER = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+
+    public static void main(String[] args) throws Exception {
+
+        /* Create a Spark program to read the airport data from in/airports.text, generate a pair RDD with airport name
+           being the key and country name being the value. Then convert the country name to uppercase and
+           output the pair RDD to out/airports_uppercase.text
+
+           Each row of the input file contains the following columns:
+
+           Airport ID, Name of airport, Main city served by airport, Country where airport is located, IATA/FAA code,
+           ICAO Code, Latitude, Longitude, Altitude, Timezone, DST, Timezone in Olson format
+
+           Sample output:
+
+           ("Kamloops", "CANADA")
+           ("Wewak Intl", "PAPUA NEW GUINEA")
+           ...
+         */
+        SparkConf conf = new SparkConf().setAppName("airports").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaPairRDD<String, String> airportsCountryUppercase = sc.textFile("src/main/resources/in/airports.text")
+                .mapToPair(line -> {
+                    String[] splits = line.split(COMMA_DELIMITER);
+                    return new Tuple2<>(splits[1], splits[3]);
+                })
+                .mapValues(String::toUpperCase);
+        airportsCountryUppercase.collect().forEach(System.out::println);
+//        airportsCountryUppercase.saveAsTextFile("src/main/resources/out/airports_uppercase_pair_rdd.text");
+
+        sc.close();
+    }
+}
